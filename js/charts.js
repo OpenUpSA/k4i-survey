febb2bfe0627cdf32653d62acbc6438c2a073d25
Adi Eyal <adi@openup.org.za>
2020-06-22 10:48:52 +0200

    Refactored code to use a class

--- js/charts.js
+++ js/charts.js
@@ -1,51 +1,56 @@
-async function loadData(url, colName) {
-    const response = await fetch(url);
-    if (response.ok) {
-        const json = await response.json();
-        const arr = [];
-        for (let i in json) {
-            arr.push(json[i][colName]);
-        }
-        const unique = Array.from(new Set(arr));
-        const count = {};
-        arr.forEach(function(i) {
-            count[i] = (count[i] || 0) + 1;
-        });
-
-        return {
-            labels: unique,
-            counts: count
-        }
-    } else {
-        console.error("Network error")
-        console.log(response)
-    }
-};
-
-function createChart(el, unique, count, chartName, chartType) {
-    var arr = [];
-    var ctx = el.getContext('2d');
-
-    for (var i in unique) {
-        var x = unique[i];
-        arr.push(count[x]);
-    }
-   
-
-    var chart = new Chart(ctx, {
-        type: chartType,
-        data: {
-            labels: [...unique],
-            datasets: [{
-                label: chartName,
-                backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe', '#ffce56', '#ff6345','#36a2gh'],
-                borderColor: 'white',
-                data: arr
-            }]
-        },
-
-        // Configuration options go here
-        options: {}
+function counts(arr) {
+    const count = {};
+    arr.forEach(function(i) {
+        count[i] = (count[i] || 0) + 1;
     });
+
+    return count
+}
+
+class K4ICharts {
+    constructor(sheet_url) {
+        this.url = sheet_url;
+    }
+
+    async loadData() {
+        const response = await fetch(this.url);
+        if (response.ok) {
+            this.data = await response.json();
+        } else {
+            throw Error(response.statusText);
+        }
+    }
+
+    extractData(col) {
+        const arr = []
+        for (let i in this.data) {
+            arr.push(this.data[i][col]);
+        }
+        
+        const count = counts(arr)
+
+        return count;
+    }
+
+    createChart(el, chartName, chartType) {
+        const ctx = el.getContext('2d');
+        const count = this.extractData(chartName);
+
+        const chart = new Chart(ctx, {
+            type: chartType,
+            data: {
+                labels: Object.keys(count),
+                datasets: [{
+                    label: chartName,
+                    backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe', '#ffce56', '#ff6345','#36a2gh'],
+                    borderColor: 'white',
+                    data: Object.values(count)
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+    }
 }
 
